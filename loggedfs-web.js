530a780d9acc57a6e469295879f374ca62b975d7
sandric <kyrychenkoanton@gmail.com>
2015-02-17 00:13:14 +0200

    remove some debugging garbage

--- loggedfs-web.js
+++ loggedfs-web.js
@@ -1,11 +1,8 @@
 var loggedfsClient = function(){
   this.terminal = require('child_process').spawn('loggedfs', ['-f', '-p', '/home', '-c', '/home/sandric/loggedfs.xml']);
-
 };
 
 loggedfsClient.prototype.start = function(onDataCallback, onEndCallback) {
-  console.log('starting...');
-  
   this.terminal.stdout.on('data', onDataCallback);
   this.terminal.on('exit', onEndCallback);
 };
