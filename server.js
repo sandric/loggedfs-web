b90be7ca318497641bfeb34bf309beee4b7fcb65
sandric <kyrychenkoanton@gmail.com>
2015-02-17 22:51:47 +0200

    refactor styles to read from file styles.css

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 var http = require('http');
+var fs = require('fs');
+
 var loggedfsClientFactory = require('./loggedfs-web.js');
 
-var eLoggedfsClient = new loggedfsClientFactory();
+fs.readFile('./styles.css', function (err, css) {
 
-http.createServer(function (req, res) {
+  if(err) throw err;
+
+  var eLoggedfsClient = new loggedfsClientFactory();
+  
+  console.log('Server running at http://127.0.0.1:1337/');
+
+  http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    
+    res.write("<style>" + css  + "</style>");
+
     eLoggedfsClient.start(function(data){
       var resultingString = "<div class = 'item'>";
       for(var el in data)
         resultingString += "<div class = '" + el + "'>" + data[el] + "</div>";
-      
+      resultingString += "</div>";
       res.write(resultingString);
     }, function(code){
     });
-}).listen(1337, '0.0.0.0');
-console.log('Server running at http://127.0.0.1:1337/');
+  }).listen(1337, '0.0.0.0');
+});
