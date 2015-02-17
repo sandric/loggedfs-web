3ace1d9b2384439802a01205cff3f2dadd571545
sandric <kyrychenkoanton@gmail.com>
2015-02-17 21:34:22 +0200

    implement parsing of output

--- loggedfs-web.js
+++ loggedfs-web.js
@@ -3,8 +3,36 @@ var loggedfsClient = function(){
 };
 
 loggedfsClient.prototype.start = function(onDataCallback, onEndCallback) {
-  this.terminal.stdout.on('data', onDataCallback);
+  var that = this;
+  this.terminal.stdout.on('data', function(data) {
+    var chunks = data.toString().split('\n');
+    for(var i in chunks) {
+      onDataCallback(that.parseOutput(chunks[i]));
+    }
+  });
+
   this.terminal.on('exit', onEndCallback);
+};
+
+loggedfsClient.prototype.parseOutput = function(outputString) {
+  var re = /(.*) \(src\/loggedfs\.cpp:\d*\) (.*?) (\/.*) \{(.*)\} \[ pid += (\d*) (.*) uid = (\d*) +\]/; 
+  var match = re.exec(outputString);
+
+  var parsedOutput;
+  if (match == null)
+    parsedOutput = outputString;
+  else
+    parsedOutput = {
+    time: match[1],
+    action: match[2],
+    attempt: match[3],
+    result: match[4] === 'SUCCESS' ? true:false,
+    pid: +match[5],
+    pname: match[6],
+    uid: +match[7]
+  };
+
+  return JSON.stringify(parsedOutput);
 };
 
 module.exports = loggedfsClient;
